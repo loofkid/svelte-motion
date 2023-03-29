31cdfa383f09521f6397e001686b515ca9898cb0
Kent Loofbourrow <kloofbourrow@aph.org>
2023-03-29 01:06:19 -0400

    oops

--- types/components/AnimatePresence/types.d.ts
+++ types/components/AnimatePresence/types.d.ts
@@ -5,7 +5,7 @@ Copyright (c) 2018 Framer B.V.
 /**
  * @public
  */
-export interface AnimatePresenceProps<T extends {[key:string]:any}> {
+export interface AnimatePresenceProps<T extends {key:any}> {
     /**
      * By passing `initial={false}`, `AnimatePresence` will disable any initial animations on children
      * that are present when the component is first rendered.
